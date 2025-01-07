f3ab72e81cc7c691711a167684bc6fc4e16cad07
Tejaswini <tejaswinijonnagaddala@gmail.com>
2025-01-07 17:51:25 +0530

    Django2

---
+++ appproject/delapp/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+def delfunction(request):
+    return render(request,'del.html')
+# Create your views here.
---
+++ appproject/projectapp/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+def projectfunction(request):
+    return render(request,'project.html')
+# Create your views here.
